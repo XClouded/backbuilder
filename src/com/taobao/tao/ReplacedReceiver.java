4a39658460d02d7401431ee4d53c06f5e8dd170a
lianheng <lianheng.myf@taobao.com>
2014-04-21 13:34:40 +0800

    覆盖安装时接收消息处理bundle

---
+++ src/com/taobao/tao/ReplacedReceiver.java
@@ -0,0 +1,18 @@
+package com.taobao.tao;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ReplacedReceiver extends BroadcastReceiver {
+
+    static final String TAG = "ReplacedReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // 应用覆盖安装时，接收消息启动TaobaoApplication，自动完成Bundle安装
+        Log.d(TAG, "onReceive: " + intent == null ? null : intent.getAction());
+    }
+
+}
