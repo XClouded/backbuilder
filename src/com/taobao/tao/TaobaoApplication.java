07f0381b9294738d38e22e9551444a215c3032f7
lianheng <lianheng.myf@taobao.com>
2014-05-05 11:03:23 +0800

    覆盖安装时更新bundles

--- src/com/taobao/tao/TaobaoApplication.java
+++ src/com/taobao/tao/TaobaoApplication.java
@@ -16,6 +16,8 @@ import org.osgi.framework.Bundle;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -23,6 +25,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ProviderInfo;
 import android.content.pm.ServiceInfo;
 import android.taobao.atlas.framework.Atlas;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.taobao.android.lifecycle.PanguApplication;
@@ -45,56 +48,113 @@ public class TaobaoApplication extends PanguApplication {
     public void onCreate() {
         super.onCreate();
 
-        // TODO: 如果当前版本大于记录版本说明客户端已经更新。
-        // 先判断lib非空即arm平台，bundle已经解压出来覆盖了原文件；
-        // lib目录为空则不是arm平台，需要我们手工将bundle文件覆盖老版本
+        long start = System.currentTimeMillis();
 
         try {
-            Properties props = new Properties();
-            props.put("android.taobao.atlas.welcome", "com.taobao.tao.welcome.Welcome");
-
             Atlas.getInstance().init(this);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not init atlas framework !!!", e);
+        }
 
-            // disableComponents(this);
+        try {
+            Field sApplication = Globals.class.getDeclaredField("sApplication");
+            sApplication.setAccessible(true);
+            sApplication.set(null, this);
+            Field sClassLoader = Globals.class.getDeclaredField("sClassLoader");
+            sClassLoader.setAccessible(true);
+            sClassLoader.set(null, Atlas.getInstance().getDelegateClassLoader());
+        } catch (Exception e) {
+            Log.e(TAG, "Could not set Globals.sApplication & Globals.sClassLoader !!!", e);
+        }
 
+        Properties props = new Properties();
+        props.put("android.taobao.atlas.welcome", "com.taobao.tao.welcome.Welcome");
+
+        // 安装程序是否已经升级了
+        boolean updated = false;
+        PackageInfo packageInfo = null;
+
+        final List<String> entryNames = new ArrayList<String>();
+
+        String processName = TaoApplication.getProcessName(Globals.getApplication());
+        if (this.getPackageName().equals(processName)) {
+
+            // 获取当前的版本号
             try {
-                Field sApplication = Globals.class.getDeclaredField("sApplication");
-                sApplication.setAccessible(true);
-                sApplication.set(null, this);
-                Field sClassLoader = Globals.class.getDeclaredField("sClassLoader");
-                sClassLoader.setAccessible(true);
-                sClassLoader.set(null, Atlas.getInstance().getDelegateClassLoader());
+                PackageManager packageManager = this.getPackageManager();
+                packageInfo = packageManager.getPackageInfo(this.getPackageName(), 0);
             } catch (Exception e) {
-                Log.e(TAG, "Could not set Globals.sApplication & Globals.sClassLoader !!!", e);
+                // 不可能发生
+                Log.e(TAG, "Error to get PackageInfo >>>", e);
+                packageInfo = new PackageInfo();
             }
 
-            Atlas.getInstance().startup(props);
+            // 检测之前的版本记录
+            SharedPreferences prefs = this.getSharedPreferences("atlas_configs", MODE_PRIVATE);
+            int lastVersionCode = prefs.getInt("last_version_code", 0);
+            String lastVersionName = prefs.getString("last_version_name", "");
 
+            // 判断版本是否更新了
+            if (packageInfo.versionCode > lastVersionCode
+                || (packageInfo.versionCode == lastVersionCode && !TextUtils.equals(packageInfo.versionName,
+                                                                                    lastVersionName))) {
+
+                updated = true;
+                // 找到安装包中所有的bundle名称，然后把磁盘上的对应bundle全部删除，以便后面重新安装新版本
+                entryNames.addAll(getBundleEntryNames("lib/armeabi/libcom_taobao", ".so"));
+                StringBuffer stringBuffer = new StringBuffer();
+                for (String entryName : entryNames) {
+                    String packageName = getPackageNameFromEntryName(entryName);
+                    if (stringBuffer.length() > 0) {
+                        stringBuffer.append(",");
+                    }
+                    stringBuffer.append(packageName);
+                }
+                String autodelete = stringBuffer.toString();
+                props.put("android.taobao.atlas.autodelete", autodelete);
+                Log.d(TAG, "autodelete: " + autodelete);
+            }
+        }
+
+        try {
+            // disableComponents(this);
+            Atlas.getInstance().startup(props);
         } catch (Exception e) {
             Log.e(TAG, "Could not start up atlas framework !!!", e);
         }
 
         // enableComponents(this);
 
-        String processName = TaoApplication.getProcessName(Globals.getApplication());
-        if (this.getPackageName().equals(processName)) {
+        Log.d(TAG, "Atlas framework started: " + (System.currentTimeMillis() - start) + " ms");
+
+        final PackageInfo fpackageInfo = packageInfo;
+        if (this.getPackageName().equals(processName) && updated) {
             Coordinator.postTask(new TaggedRunnable("ProcessBundles") {
 
                 @Override
                 public void run() {
-                    processLibsBundles();
+                    long start = System.currentTimeMillis();
+                    
+                    processLibsBundles(entryNames);
                     // 或许有Bundle新增或更新，再次刷新Component的状态
                     // enableComponents(TaobaoApplication.this);
                     Log.d(TAG, "sendStickyBroadcast: com.taobao.taobao.action.BUNDLES_INSTALLED");
                     TaobaoApplication.this.sendStickyBroadcast(new Intent("com.taobao.taobao.action.BUNDLES_INSTALLED"));
+
+                    SharedPreferences prefs = TaobaoApplication.this.getSharedPreferences("atlas_configs", MODE_PRIVATE);
+                    Editor editor = prefs.edit();
+                    editor.putInt("last_version_code", fpackageInfo.versionCode);
+                    editor.putString("last_version_name", fpackageInfo.versionName);
+                    editor.commit();
+                    
+                    Log.d(TAG, "Updated bundles: " + (System.currentTimeMillis() - start) + " ms");
                 }
             });
         }
 
     }
 
-    private void processLibsBundles() {
-        List<String> entryNames = getBundleEntryNames("lib/armeabi/libcom_taobao", ".so");
+    private void processLibsBundles(List<String> entryNames) {
         // 首先按照预先设定的顺序处理Bundle安装包
         for (int i = 0; i < SORTED_PACKAGES.length; i++) {
             String pkg = SORTED_PACKAGES[i].replace(".", "_");
@@ -112,20 +172,15 @@ public class TaobaoApplication extends PanguApplication {
 
     private boolean processLibsBundle(String entryName) {
 
-        String fileName = entryName.substring(entryName.indexOf("lib/armeabi/") + "lib/armeabi/".length());
-        String packageName = entryName.substring(entryName.indexOf("lib/armeabi/lib") + "lib/armeabi/lib".length(),
-                                                 entryName.indexOf(".so"));
-        packageName = packageName.replace("_", ".");
+        String fileName = getFileNameFromEntryName(entryName);
+        String packageName = getPackageNameFromEntryName(entryName);
 
         File libDir = new File(getFilesDir().getParentFile(), "lib");
         File soFile = new File(libDir, fileName);
 
         Bundle bundle = Atlas.getInstance().getBundle(packageName);
-        if (bundle != null) {
-            // TODO: 检测是否需要更新
-
-        } else {
-            // 新增Bundle,需要安装
+        if (bundle == null) {
+            // 由于需要更新的bundle已经在容器启动时删除了，这里只要安装bundle就好了
             try {
                 InputStream inputStream = null;
                 if (soFile.exists()) {
@@ -157,6 +212,18 @@ public class TaobaoApplication extends PanguApplication {
             }
         }
         return false;
+    }
+
+    private String getFileNameFromEntryName(String entryName) {
+        String fileName = entryName.substring(entryName.indexOf("lib/armeabi/") + "lib/armeabi/".length());
+        return fileName;
+    }
+
+    private String getPackageNameFromEntryName(String entryName) {
+        String packageName = entryName.substring(entryName.indexOf("lib/armeabi/lib") + "lib/armeabi/lib".length(),
+                                                 entryName.indexOf(".so"));
+        packageName = packageName.replace("_", ".");
+        return packageName;
     }
 
     private String filterEntryName(List<String> entryNames, String packageName) {
