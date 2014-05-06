ffcc919e9625b2c65b573c89eb634f20dd2947cb
zeyin <zeyin@taobao.com>
2014-05-07 00:45:41 +0800

    add awblib

--- install_build.sh
+++ install_build.sh
@@ -11,6 +11,9 @@ MVN_HOME_PRJ=$2
 MVN_D_FILE=$3
 ROOT_PATH=`pwd`
 BUILD_GIT_CONF_FILE="$ROOT_PATH/git.list"
+BUILD_GIT_CONF_FILE_APKLIB="$ROOT_PATH/git.list.apklib"
+BUILD_GIT_CONF_FILE_AAR="$ROOT_PATH/git.list.aar"
+BUILD_GIT_CONF_FILE_AWB="$ROOT_PATH/git.list.awb"
 BUILD_PATH="$ROOT_PATH/build-project"
 MVN_REPO_LOCAL="$ROOT_PATH/build-repo"
 ERR_RET=`mvn -v|awk '{print $3}'`
@@ -19,10 +22,6 @@ if [  $MVN_HOME_PRJ ]; then
 	export MAVEN_HOME=$MVN_HOME_PRJ
 	export PATH=$MAVEN_HOME/bin:$PATH
 	echo ">>Current Maven is $MAVEN_HOME"
-fi
-if [ $MVN_D_FILE -a -f $MVN_D_FILE ]; then
-	$BUILD_GIT_CONF_FILE=$MVN_D_FILE
-	echo ">>current build file:$BUILD_GIT_CONF_FILE"
 fi
 
 
@@ -48,7 +47,7 @@ function build_taobaocompat(){
 }
 
 
-function doBundleBuilder(){
+function do_jar_build(){
 	echo ">>start to build bundle"
 	cd $BUILD_PATH
 	git_list=$(cat $BUILD_GIT_CONF_FILE)
@@ -71,7 +70,76 @@ function doBundleBuilder(){
 	done
 }
 
-function doBuilder(){
+function do_apklib_build(){
+        echo ">>start to build bundle"
+        cd $BUILD_PATH
+        git_list=$(cat $BUILD_GIT_CONF_FILE_APKLIB)
+        while read line ; do
+                param_b=`echo $line | grep  -o ' \-b '`
+                if [ $param_b ]; then
+                        git clone $line
+                else
+                        git clone $line -b $BRANCH
+                fi
+        done < $BUILD_GIT_CONF_FILE
+
+        for file in `ls $BUILD_PATH`
+        do
+            if  test -d $file ; then
+                        echo ">>start to install in $file"
+                cd $BUILD_PATH/$file
+                        mvn install -e $MVN_OPT -Papklib
+            fi
+        done
+}
+
+function do_aar_build(){
+        echo ">>start to build bundle"
+        cd $BUILD_PATH
+        git_list=$(cat $BUILD_GIT_CONF_FILE_AAR)
+        while read line ; do
+                param_b=`echo $line | grep  -o ' \-b '`
+                if [ $param_b ]; then
+                        git clone $line
+                else
+                        git clone $line -b $BRANCH
+                fi
+        done < $BUILD_GIT_CONF_FILE
+
+        for file in `ls $BUILD_PATH`
+        do
+            if  test -d $file ; then
+                        echo ">>start to install in $file"
+                cd $BUILD_PATH/$file
+                        mvn install -e $MVN_OPT -Paar
+            fi
+        done
+}
+
+function do_awb_build(){
+        echo ">>start to build bundle"
+        cd $BUILD_PATH
+        git_list=$(cat $BUILD_GIT_CONF_FILE_AWB)
+        while read line ; do
+                param_b=`echo $line | grep  -o ' \-b '`
+                if [ $param_b ]; then
+                        git clone $line
+                else
+                        git clone $line -b $BRANCH
+                fi
+        done < $BUILD_GIT_CONF_FILE
+
+        for file in `ls $BUILD_PATH`
+        do
+            if  test -d $file ; then
+                        echo ">>start to install in $file"
+                cd $BUILD_PATH/$file
+                        mvn install -e $MVN_OPT -Pawb
+            fi
+        done
+}
+
+function do_builder(){
 	echo "start to builder apk main"
 	cd $ROOT_PATH
 	mvn clean package -e $MVN_OPT
@@ -79,8 +147,11 @@ function doBuilder(){
 
 init_path;
 build_taobaocompat;
-doBundleBuilder;
-doBuilder;
+do_jar_build;
+do_apklib_build;
+do_aar_build;
+do_awb_build;
+do_builder;
 
 
 
