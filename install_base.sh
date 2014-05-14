4947293e6807fc24990e439de321881255ffdf44
zeyin <zeyin@taobao.com>
2014-05-15 02:57:21 +0800

    fix shell

--- install_base.sh
+++ install_base.sh
@@ -138,7 +138,6 @@ function do_awb_build(){
         cd $BUILD_PATH_AWB
         git_list=$(cat $BUILD_GIT_CONF_FILE_AWB)
         while read line ; do
-	#echo $line
                 param_b=`echo $line | grep  -o ' \-b '`
                 if [ $param_b ]; then
                         git clone $line
@@ -148,7 +147,7 @@ function do_awb_build(){
         done < $BUILD_GIT_CONF_FILE_AWB
         for file in `ls $BUILD_PATH_AWB`
         do
-            if  test -d $file ; then
+            if  test -d $BUILD_PATH_AWB/$file ; then
               echo ">>start to install in $file"
               cd $BUILD_PATH_AWB/$file
               mvn install -e $MVN_OPT -Pawb -Dproguard.skip=false
