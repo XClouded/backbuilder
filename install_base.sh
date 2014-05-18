313c9951468a777ffc0f35bcff54cf8b15e2226a
zeyin <zeyin@taobao.com>
2014-05-19 01:16:27 +0800

    add exit for build error

--- install_base.sh
+++ install_base.sh
@@ -96,12 +96,12 @@ function build_taobaocompat(){
   mvn install -U -e $MVN_OPT -Papklib
   if [ $? -ne 0 ]; then
         echo "build compat error!"
-        ##exit $?
+        exit $?
   fi
   mvn install -U -e $MVN_OPT -Paar
   if [ $? -ne 0 ]; then
         echo "build compat error!"
-        ##exit $?
+        exit $?
   fi
 }
 
@@ -130,7 +130,7 @@ function do_jar_build(){
         mvn install -e $MVN_OPT
         if [ $? -ne 0 ]; then
               echo "build $file error!"
-              ##exit $?
+              exit $?
         fi
       fi
   done
@@ -160,7 +160,7 @@ function do_apklib_build(){
                 mvn install -e $MVN_OPT -Papklib
                 if [ $? -ne 0 ]; then
                       echo "build $file error!"
-                      ##exit $?
+                      exit $?
                 fi
             fi
         done
@@ -194,7 +194,7 @@ function do_aar_build(){
                 mvn install -e $MVN_OPT -Paar
                 if [ $? -ne 0 ]; then
                       echo "build $file error!"
-                      ##exit $?
+                      exit $?
                 fi
             fi
         done
@@ -225,7 +225,7 @@ function do_awb_build(){
               mvn install -e $MVN_OPT -Pawb
               if [ $? -ne 0 ]; then
                     echo "build $file error!"
-                    ##exit $?
+                    exit $?
               fi
             fi
         done
