1d8839a81cfc8c78c1b8bb23356495250fc094e6
zeyin <zeyin@taobao.com>
2014-05-18 17:10:06 +0800

    a

--- install_build.sh
+++ install_build.sh
@@ -3,10 +3,10 @@
 ##引入基础shell
 source install_base.sh
 
-build_taobaocompat;
-do_jar_build;
-do_apklib_build;
-do_aar_build;
-do_awb_build;
-do_awb_svn;
+#build_taobaocompat;
+#do_jar_build;
+#do_apklib_build;
+#do_aar_build;
+#do_awb_build;
+#do_awb_svn;
 do_builder;
