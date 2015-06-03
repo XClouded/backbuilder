12a877dd2a3a11d193d2f75a1753a26a1549a64c
xieguo.xg <xieguo.xg@alibaba-inc.com>
2015-06-03 06:03:49 -0700

    Remove passivelocation from autostart bundle list temp.
    Signed-off-by: xieguo.xg <xieguo.xg@alibaba-inc.com>

--- src/com/taobao/tao/atlaswrapper/AutoStartBundlesLaunch.java
+++ src/com/taobao/tao/atlaswrapper/AutoStartBundlesLaunch.java
@@ -24,7 +24,7 @@ class AutoStartBundlesLaunch {
 	private final static String TAG = "AutoStartBundlesLaunch";
     private final String[] asyncBundleDbg = {"com.taobao.barrier"};
 	private final String[] asyncBundle = {"com.taobao.taobao.home", "com.taobao.allspark", "com.taobao.login4android"};
-	private final String[] delayBundle = {"com.taobao.wangxin", "com.taobao.passivelocation", "com.taobao.tao.contacts", "com.taobao.tbpoplayer"};
+	private final String[] delayBundle = {"com.taobao.wangxin", "com.taobao.tao.contacts", "com.taobao.tbpoplayer"};
 	private final String[] delayBundleOnXiaoMi = {"com.taobao.xiaomi"};
 	private HomeFinishedBroadcastReceiver receiver;
 	private boolean isAsyncStarted = false;
