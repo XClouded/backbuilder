6c6083485745090a11bceacd67637bd1768d2b56
关杰 <guanjie.yjf@taobao.com>
2015-09-26 17:38:27 +0800

    打trace进行保护

--- src/com/taobao/tao/frameworkwrapper/AtlasMonitorImpl.java
+++ src/com/taobao/tao/frameworkwrapper/AtlasMonitorImpl.java
@@ -3,15 +3,21 @@ package com.taobao.tao.frameworkwrapper;
 import android.taobao.atlas.util.IMonitor;
 import com.taobao.statistic.TBS;
 
+import java.lang.Throwable;
+
 /**
  * Created by guanjie on 15/7/8.
  */
 public class AtlasMonitorImpl implements IMonitor{
     public void trace(String num, String arg1, String arg2, String detail){
-        TBS.Ext.commitEvent(61005, num, arg1, arg2, detail);
+        try {
+            TBS.Ext.commitEvent(61005, num, arg1, arg2, detail);
+        }catch(Throwable e){e.printStackTrace();}
     }
 
     public void trace(Integer num, String arg1, String arg2, String detail){
+        try {
         TBS.Ext.commitEvent(61005, num.toString(), arg1, arg2, detail);
+        }catch(Throwable e){e.printStackTrace();}
     }
 }
