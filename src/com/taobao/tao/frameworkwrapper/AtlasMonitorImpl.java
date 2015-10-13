d9309e6685be487b1ddd43b9a9a41d6a60f03e06
xieguo.xg <xieguo.xg@alibaba-inc.com>
2015-10-13 14:32:23 +0800

    Add a dimension for disk remains.
    Signed-off-by: xieguo.xg <xieguo.xg@alibaba-inc.com>

--- src/com/taobao/tao/frameworkwrapper/AtlasMonitorImpl.java
+++ src/com/taobao/tao/frameworkwrapper/AtlasMonitorImpl.java
@@ -11,6 +11,8 @@ import com.alibaba.mtl.appmonitor.model.MeasureValueSet;
 import com.taobao.tao.TaoPackageInfo;
 import com.taobao.tao.util.GetAppKeyFromSecurity;
 
+import java.lang.Exception;
+
 /**
  * Created by guanjie on 15/7/8.
  */
@@ -23,7 +25,7 @@ public class AtlasMonitorImpl implements IMonitor{
 		AppMonitor.init(application);
 		AppMonitor.setRequestAuthInfo(true, GetAppKeyFromSecurity.getAppKey(0),null);
 		AppMonitor.setChannel(TaoPackageInfo.getTTID());
-		DimensionSet dimensionSet = DimensionSet.create().addDimension("TypeID").addDimension("Detail").addDimension("BundleName");
+		DimensionSet dimensionSet = DimensionSet.create().addDimension("TypeID").addDimension("Detail").addDimension("BundleName").addDimension("diskLeverage");
 		MeasureSet measureSet = MeasureSet.create().addMeasure("remainDisk");
 		AppMonitor.register("Atlas", "Monitor", measureSet, dimensionSet);
             } catch (Exception e){
@@ -32,17 +34,41 @@ public class AtlasMonitorImpl implements IMonitor{
 	
 	public void trace(String TypeID, String BundleName, String Detail, String remainedDisk){
             try{
-		AppMonitor.Stat.commit("Atlas", "Monitor", interactiveDSet.setValue("TypeID", TypeID).setValue("Detail",Detail).setValue("BundleName", BundleName),
-                interactiveMSet.setValue("remainDisk", Double.parseDouble(remainedDisk)));
+		AppMonitor.Stat.commit("Atlas", "Monitor",
+				interactiveDSet.setValue("TypeID", TypeID).setValue("Detail", Detail).setValue("BundleName", BundleName).setValue("diskLeverage", getDiskLeverage(remainedDisk)),
+				interactiveMSet.setValue("remainDisk", Double.parseDouble(remainedDisk)));
             } catch (Exception e){
             }
 	}
 	
 	public void trace(Integer TypeID, String BundleName, String Detail, String remainedDisk){
             try{
-		AppMonitor.Stat.commit("Atlas", "Monitor", interactiveDSet.setValue("TypeID", TypeID.toString()).setValue("Detail",Detail).setValue("BundleName", BundleName),
-                interactiveMSet.setValue("remainDisk", Double.parseDouble(remainedDisk)));
+		AppMonitor.Stat.commit("Atlas", "Monitor",
+				interactiveDSet.setValue("TypeID", TypeID.toString()).setValue("Detail",Detail).setValue("BundleName", BundleName).setValue("diskLeverage", getDiskLeverage(remainedDisk)),
+				interactiveMSet.setValue("remainDisk", Double.parseDouble(remainedDisk)));
             } catch (Exception e){
             }
+	}
+
+	private String getDiskLeverage(String remainedDisk){
+		String leverage = "";
+		try {
+			long diskSize = Long.parseLong(remainedDisk);
+
+			if (diskSize <= 50) {
+				leverage = "0-50";
+			} else if (diskSize <= 100) {
+				leverage = "50-100";
+			} else if (diskSize <= 200) {
+				leverage = "100-200";
+			} else if (diskSize <= 500) {
+				leverage = "200-500";
+			} else {
+				leverage = "500+";
+			}
+		}catch (Exception e){
+		}
+
+		return leverage;
 	}
 }
